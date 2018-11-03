a4425a62fb725097f4131ba727fe396aacd32f19
Rohan Jhunjhunwala <rjhunjhunwala80@berkeley.edu>
2018-11-03 00:59:33 -0700

    added improvements

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from flask import Flask
 # import io
 # from imageio import imread
 
-app = Flask(__name__, static_url_path='/static/')
-app._set_static_folder("static")
+app = Flask(__name__)
+app.static_url_path = "/static"
+app.send_static_file("/static/profile.png")
 
 @app.route('/make_new/<id>')
 def make_new(id):
@@ -14,8 +15,6 @@ def make_new(id):
 @app.route('/<id>')
 def get_notes_page(id):
     return id
-
-
 
 if __name__ == '__main__':
     app.run()
