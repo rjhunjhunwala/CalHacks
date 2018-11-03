bbd123b2edfca3997f569bbe58b932d613818341
Rohan Jhunjhunwala <rjhunjhunwala80@berkeley.edu>
2018-11-03 15:13:51 -0700

    added improvements

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ output_text = ""
 @app.route('/<id>')
 def get_notes_page(id):
     if id[0:9] == "API_INPUT":
-        return make_file(None)
+        return make_file(get_parsed_data(None))
         img = get_image_from_sketch_back_end(id[9:])
         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         output = get_parsed_data(img)
