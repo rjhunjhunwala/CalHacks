3daca3e64555df96da35b38470a58b37b275057a
Rohan Jhunjhunwala <rjhunjhunwala80@berkeley.edu>
2018-11-03 00:20:52 -0700

    added improvements

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask
 import base64
-# import cv2
+import cv2
 import io
 from imageio import imread
 
 app = Flask(__name__)
 
-"""
+
 @app.route('/make_new/<id>')
 def hello_world(id):
     image = get_parsed_data(imread(io.BytesIO(base64.b64decode(id))))
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     cv2.imwrite("last_image",image)
     return '<img href="last_image"></img>'
-"""
+
 @app.route('/<id>')
 def hello_world(id):
     return id
