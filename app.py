84902abbe8047c72eb40e46f27f4ba72bec68436
Rohan Jhunjhunwala <rjhunjhunwala80@berkeley.edu>
2018-11-03 00:53:56 -0700

    added improvements

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask
 # from imageio import imread
 
 app = Flask(__name__, static_url_path='/static/')
-
+app._set_static_folder("static")
 
 @app.route('/make_new/<id>')
 def make_new(id):
