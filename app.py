ff78fef18ad53460d5ceaa787634c7d2d02f8864
Rohan Jhunjhunwala <rjhunjhunwala80@berkeley.edu>
2018-11-03 00:24:13 -0700

    added improvements

--- app.py
+++ app.py
@@ -9,10 +9,7 @@ app = Flask(__name__)
 
 @app.route('/make_new/<id>')
 def hello_world(id):
-    image = get_parsed_data(imread(io.BytesIO(base64.b64decode(id))))
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    cv2.imwrite("last_image",image)
-    return '<img href="last_image"></img>'
+    return '<img href="profile.png"></img>'
 
 @app.route('/<id>')
 def hello_world(id):
