125694a1159dae232d507d17c65df411b94edef8
Rohan Jhunjhunwala <rjhunjhunwala80@berkeley.edu>
2018-11-03 00:41:32 -0700

    fixed

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/make_new/<id>')
 def make_new(id):
-    return '<img src="profile"></img>'
+    return '<img src="profile.png"></img>'
 
 @app.route('/<id>')
 def get_notes_page(id):
