72a2e0a5b3da36f91f5ae54b440f694b5d09d2b7
Rohan Jhunjhunwala <rjhunjhunwala80@berkeley.edu>
2018-11-03 11:06:48 -0700

    added improvements

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 # import io
 # from imageio import imread
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__)
 
 @app.route('/make_new/<id>')
 def make_new(id):
@@ -14,6 +14,15 @@ def make_new(id):
 def get_notes_page(id):
     return render_template("index.html")
 
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    return response
 if __name__ == '__main__':
     app.run()
 
