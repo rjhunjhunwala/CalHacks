ca72b6b7571955c5f8dd4f7b28154290fdc37068
Rohan Jhunjhunwala <rjhunjhunwala80@berkeley.edu>
2018-11-02 23:25:36 -0700

    cv2

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ app = Flask(__name__)
 
 @app.route('/<id>')
 def hello_world(id):
-
+    return id
 
 
 if __name__ == '__main__':
     app.run()
+
+def get_black_box(img):
+    """
+
+    :param img: the
+    :return:
+    """
