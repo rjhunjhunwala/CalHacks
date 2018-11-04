e2407171fb6ee0bc0d71e02d5c67c0e1de97d57f
Kushagra Pandey <kushagrapandey@Kushagras-MacBook-Air.local>
2018-11-04 10:29:58 -0800

    integrated CV

--- app.py
+++ app.py
@@ -5,13 +5,20 @@ from imageio import imread
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
+from four_point_transform import four_point_transform
 
 from apiclient import errors
 from apiclient.http import MediaFileUpload, MediaIoBaseDownload
 import io
 import tensorflow as tf
 import keras
-import numpy
+from skimage.filters import threshold_local
+from PIL import Image, ImageOps, ImageDraw
+from scipy.ndimage import morphology, label
+import pytesseract
+from wrapper import *
+from random import randint
+import numpy as np
 import scipy
 import pickle
 import imutils
@@ -140,6 +147,196 @@ def get_parsed_data(img):
     :param img: CV2 image that's BGR
     :return:
     """
-    return [["HEAD","Thevenin Equivalents"],["IMAGE",["https://cdn.instructables.com/FZY/8TLQ/IMF5Z55H/FZY8TLQIMF5Z55H.LARGE.jpg"]],["TEXT", "This is a Thevenin Equivalent. "],
-            ["HEAD","Norton"],["IMAGE",["https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/NortonEquivalentCircuits.png/1200px-NortonEquivalentCircuits.png"]],["TEXT", "This is a norton equivalent works."],[
-                "HEAD","OP-AMPS"],["IMAGE",["https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/OpAmpTransistorLevel_Colored_Labeled.svg/780px-OpAmpTransistorLevel_Colored_Labeled.svg.png","https://cdn.sparkfun.com/assets/learn_tutorials/6/5/2/opamp.png"]]]
+    cv2_im = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    pil_im = Image.fromarray(cv2_im)
+    ratio = image.shape[0]/500.0
+    orig = image.copy()
+    image = imutils.resize(image, height=500)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5,5), 0)
+    def autocanny(image, sigma=0.33):
+        v = np.median(image)
+        lower = int(max(0, (1.0-sigma)*v))
+        upper = int(min(255, (1.0+sigma)*v))
+        edged = cv2.Canny(image, lower, upper)
+        return edged
+    edged = autocanny(gray)
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+    for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02*peri, True)
+        if len(approx)==4:
+            screenCnt = approx
+            break
+    warped = four_point_transform(orig, screenCnt.reshape(4,2)*ratio)
+    warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+    T = threshold_local(warped, 11, offset=10, method="gaussian")
+    warped = (warped>T).astype("uint8")*255
+    height, width = warped.shape
+    warped = warped[int(0.05*height): int(0.95*height), int(0.01*width): int(0.99*width)]
+    gray = cv2.GaussianBlur(warped, (5,5), 0)
+    edged = autocanny(gray)
+    warped = cv2.cvtColor(warped,cv2.COLOR_GRAY2RGB)
+    kernel = np.ones((3,3),np.uint8)
+    edged = cv2.dilate(edged, kernel, iterations=1)
+    thresh_area = 50
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    pil_im = Image.fromarray(warped)
+    class Line():
+        box=False
+        def __init__(self, coordinates):
+            self.coordinates = coordinates
+    class Box():
+        box=True
+        def __init__(self, coordinates):
+            self.coordinates = coordinates
+    def boxes(orig):
+        img = ImageOps.grayscale(orig)
+        im = np.array(img)
+        
+        # Inner morphological gradient.
+        im = morphology.grey_dilation(im, (3, 3)) - im
+        
+        # Binarize.
+        mean, std = im.mean(), im.std()
+        t = mean + std
+        im[im < t] = 0
+        im[im >= t] = 1
+        
+        # Connected components.
+        lbl, numcc = label(im)
+        # Size threshold.
+        min_size = 200 # pixels
+        box = []
+        print("Number of connected components is: " + str(numcc))
+        for i in range(1, numcc + 1):
+            py, px = np.nonzero(lbl == i)
+            if len(py) < min_size:
+                im[lbl == i] = 0
+                continue
+            
+            xmin, xmax, ymin, ymax = px.min(), px.max(), py.min(), py.max()
+            # Four corners and centroid.
+            if (ymax-ymin) * (xmax-xmin) < 200000:
+                continue
+            box.append([
+                        [(xmin, ymin), (xmax, ymin), (xmax, ymax), (xmin, ymax)],
+                        (np.mean(px), np.mean(py))])
+        return im.astype(np.uint8) * 255, box
+    orig = pil_im
+    im, box = boxes(orig)
+    elements = []
+    for c in cnts:
+        rect = cv2.minAreaRect(c)
+        min_box = cv2.cv.BoxPoints(rect) if imutils.is_cv2() else cv2.boxPoints(rect)
+        min_box = np.int0(min_box)
+        min_x = min(p[0] for p in min_box)
+        max_x = max(p[0] for p in min_box)
+        min_y = min(p[1] for p in min_box)
+        max_y = max(p[1] for p in min_box)
+        flag = True
+        if cv2.contourArea(c)>100:
+            for b, centroid in box:
+                if min_x <= b[1][0] and max_x <= b[1][0] and min_x >= b[0][0] and max_x>=b[0][0] and min_y <= b[2][1] and max_y<= b[2][1] and min_y>= b[0][1] and max_y>=b[0][1]:
+                    flag=False
+            if flag:
+                if (max_x-min_x)>450 and (max_y-min_y)<= 100:
+                    cv2.drawContours(warped, [min_box], -1, (0,255,0),3)
+                    elements.append(Line([min_x, max_x, min_y, max_y]))
+        else:
+            continue
+    for b, centroid in box:
+        cv2.rectangle(warped, (b[0][0], b[0][1]), (b[1][0], b[2][1]), (0,255,0))
+        elements.append(Box([b[0][0], b[1][0], b[0][1], b[3][1]]))
+    elements.sort(key=lambda element: element.coordinates[2])
+    toReturn = []
+    i=0
+    while i<len(elements):
+        if not elements[i].box:
+            img = warped[elements[i].coordinates[3] - 300 :elements[i].coordinates[3], 0:elements[i].coordinates[1]]
+            pillow = Image.fromarray(img)
+            text = pytesseract.image_to_string(pillow)
+            for char in text:
+                if not char.isalnum():
+                    text = text.replace(char,"")
+                print(text)
+                toReturn.append(["HEAD",text])
+                i+=1
+        else:
+            j=i
+            while elements[j].box and j!=len(elements)-1:
+                j+=1
+            if not elements[j].box:
+                if i+2==j:
+                    min_y = max(elements[i].coordinates[3], elements[i+1].coordinates[3])
+                    img = warped[min_y:elements[j].coordinates[2]-300 , 0:warped.shape[:2][1]]
+                    cv2.imwrite("passed_in1.png", img)
+                    pillow = Image.fromarray(img)
+                    text = pytesseract.image_to_string(pillow)
+                    img1 = warped[elements[i].coordinates[2]: elements[i].coordinates[3],elements[i].coordinates[0]:elements[i].coordinates[1]]
+                    x = randint(1, 1000000)
+                    cv2.imwrite(str(x)+ ".jpg", img1)
+                    upload_file(str(x)+ ".jpg") #generate large number
+                    url1 = get_base_url()+get_id_from_title(str(x)+ ".jpg")
+                    img2 = warped[elements[i+1].coordinates[2]: elements[i+1].coordinates[3], elements[i+1].coordinates[0]:elements[i+1].coordinates[1]]
+                    x = randint(1, 1000000)
+                    cv2.imwrite(str(x)+ ".jpg", img2)
+                    upload_file(str(x)+ ".jpg") #generate large number
+                    url2 = get_base_url()+get_id_from_title(str(x)+ ".jpg")
+                    toReturn.append(["IMAGE", [url1, url2]])
+                    toReturn.append(["TEXT", text])
+                    print(text)
+                    i+=2
+                elif i+1==j:
+                    img = warped[elements[i].coordinates[3]:elements[j].coordinates[2]-300 , 0:warped.shape[:2][1]]
+                    pillow = Image.fromarray(img)
+                    text = pytesseract.image_to_string(pillow)
+                    img1 = warped[elements[i].coordinates[2]: elements[i].coordinates[3],elements[i].coordinates[0]:elements[i].coordinates[1]]
+                    x = randint(1, 1000000)
+                    cv2.imwrite(str(x)+ ".jpg", img1)
+                    upload_file(str(x)+ ".jpg") #generate large number
+                    url = get_base_url()+get_id_from_title(str(x)+ ".jpg")
+                    toReturn.append(["IMAGE", [url]])
+                    toReturn.append(["TEXT", text])
+                    print(text)
+                    i+=1
+            else:
+                if i<j:
+                    min_y = max(elements[i].coordinates[3], elements[i+1].coordinates[3])
+                    img = warped[min_y:warped.shape[:2][0], 0:warped.shape[:2][1]]
+                    cv2.imwrite("passed_in.png", img)
+                    pillow = Image.fromarray(img)
+                    text = pytesseract.image_to_string(pillow)
+                    img1 = warped[elements[i].coordinates[2]: elements[i].coordinates[3], elements[i].coordinates[0]:elements[i].coordinates[1]]
+                    x = randint(1, 1000000)
+                    cv2.imwrite(str(x)+ ".jpg", img1)
+                    upload_file(str(x)+ ".jpg") #generate large number
+                    url1 = get_base_url()+get_id_from_title(str(x)+ ".jpg")
+                    img2 = warped[elements[i+1].coordinates[2]: elements[i+1].coordinates[3], elements[i+1].coordinates[0]:elements[i+1].coordinates[1]]
+                    x = randint(1, 1000000)
+                    cv2.imwrite(str(x)+ ".jpg", img2)
+                    upload_file(str(x)+ ".jpg") #generate large number
+                    url2 = get_base_url()+get_id_from_title(str(x)+ ".jpg")
+                    toReturn.append(["IMAGE", [url1, url2]])
+                    toReturn.append(["TEXT", text])
+                    print(text)
+                    i+=2
+                else:
+                    img = warped[elements[i].coordinates[3]:warped.shape[:2][0], 0:warped.shape[:2][1]]
+                    cv2.imwrite("passed_in.png", img)
+                    pillow = Image.fromarray(img)
+                    text = pytesseract.image_to_string(pillow)
+                    img1 = warped[elements[i].coordinates[2]: elements[i].coordinates[3],elements[i].coordinates[0]:elements[i].coordinates[1]]
+                    x = randint(1, 1000000)
+                    cv2.imwrite(str(x)+ ".jpg", img1)
+                    upload_file(str(x)+ ".jpg") #generate large number
+                    url = get_base_url()+get_id_from_title(str(x)+ ".jpg")
+                    toReturn.append(["IMAGE", [url]])
+                    toReturn.append(["TEXT", text])
+                    print(text)
+                    i+=1
+
+    return toReturn
